5dcf088664e0d9f375f1e769e99ce2a27ec00ed1
MG2496 <michaelgamboamoya24@gmail.com>
2022-04-04 19:53:12 -0600

    completed

--- App.js
+++ App.js
@@ -4,7 +4,10 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={[styles.welcome, {flex: 5, backgroundColor: 'orange'},{color: 'white'}]}>Hellos React Native 🫡</Text>
+      <Text style={[styles.welcome, {flex: 4, backgroundColor: 'gray'},{color: 'white'}]}>Hellos React Native 🤓</Text>
+      <Text style={[styles.welcome, {flex: 3, backgroundColor: 'black'},{color: 'white'}]}>Hellos React Native 🫥</Text>
+      <Text style={[styles.welcome, {flex: 2, backgroundColor: 'green'},{color: 'white'}]}>Hellos React Native 😵‍💫</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +16,15 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+    //alignItems: 'center',
+    backgroundColor: 'brown'
+  },
+  welcome: {
+    fontSize: 40,
+    textAlign: 'center',
+    
+    margin: 10,
+
   },
 });
